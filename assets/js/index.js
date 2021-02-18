e821839ef5a2d096700f632017b5ce4f603593fe
O-luwanifemi <66732138+O-luwanifemi@users.noreply.github.com>
2021-02-18 11:35:50 +0100

    amended the markup, and css, to harbour new button

---
+++ assets/js/index.js
@@ -0,0 +1,8 @@
+// NEEDED VARIABLES
+// let add_btn = document.querySelector(".add-btn");
+
+// add_btn.addEventListener('click', function() {
+//     alert('Yes! We did it');
+// })
+
+alert('Hello, world')
