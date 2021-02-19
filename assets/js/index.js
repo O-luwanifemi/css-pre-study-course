a0700721207462883bf4b31d8985ca5149df34b2
O-luwanifemi <66732138+O-luwanifemi@users.noreply.github.com>
2021-02-19 01:34:30 +0100

    made more success adding js functionalities to project

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,11 @@
-alert('Helo');
-
 // ----- NEEDED VARIABLES -----
 let modal = document.getElementById('post-modal');
 let close_btn = document.querySelector('.close');
+let form_submit_btn = document.querySelector('.submit_btn');
 let form_submit = document.getElementById('form');
 let add_btn = document.querySelector(".add-btn");
 let post_count = document.getElementById("article-count");
 let articles_holder = document.getElementById("articles");
-let articles = document.querySelectorAll("#articles article");
 
 // ----- ALL EVENT LISTENERS -----
 add_btn.addEventListener('click', showModal);
@@ -35,37 +33,48 @@ function submitPost(event) {
     // Variables to store inputs
     let title = document.getElementById('post-title').value;
     let post_body = document.getElementById('post-body').value;
+    let date = new Date();
 
-    // Creating e to contain new posts
+    if (title == '' || post_body == '') {
+        form_submit_btn.className += " disabled";
+    }
+    
+    // Setting up structure for incoming posts
     let article = document.createElement('article');
         article.className += "article top-article";
+        article.innerHTML = `<section class="top-text-content">
+                                <h2 class="journal_title"><span class="date">${date.toLocaleDateString("en-GB")}:</span> ${title}</h2>
+                                <p>
+                                    ${post_body}
+                                </p>
+                            </section>
 
-    let article = document.createElement('article');
-        article.className += "article top-article";
+                            <div class="top-img-box">
+                                <img src="./assets/images/spatula.gif" alt="spatulas">
+                            </div>`;
 
+    // strings together the new content
+    articles_holder.prepend(article);
 
+    // Calls for count update
+    updateArticleCount();
 
-
-    // <article class="article top-article">
-    //     <section class="top-text-content">
-    //         <h2 class="journal_title"><span class="date">13/05/23:</span> Spatulas</h2>
-    //         <p>
-    //             Yesterday I went to the store and got some much-needed <a href="https://www.youtube.com/watch?v=2XbCWmY0eqY">spatulas!</a> (What better way to say I love myself than to buy myself a spatula?)
-    //         </p>
-    //     </section>
-
-    //     <div class="top-img-box">
-    //         <img src="./assets/images/spatula.gif" alt="spatulas">
-    //     </div>
-    // </article>
-
+    // Clears out the just-posted inputs
+    document.forms['form'].reset();
 }
 
-// DETERMIMING COUNT BASED ON POSTED ARTICLE COUNT
-if (articles.length === 0) {
-    post_count.textContent = "0 posts.";
-} else if (articles.length === 1) {
-    post_count.textContent = "1 post.";
-} else {
-    post_count.textContent = `${articles.length} posts.`;
+// ----- Updating count based on number of posts on UI -----
+// Setting default count, due to hard-coded articles already on screen
+post_count.textContent = "2 posts";
+
+function updateArticleCount() {
+    let articles = articles_holder.childElementCount;
+
+    if (articles === 0) {
+        post_count.textContent = "0 posts.";
+    } else if (articles === 1) {
+        post_count.textContent = "1 post.";
+    } else {
+        post_count.textContent = `${articles} posts.`;
+    }
 }
